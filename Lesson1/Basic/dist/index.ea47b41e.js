03dd3f587fcdc1c8242c729b353a94a491b15f24
Rajashree Rajesh Arvikar <79647432+rajashreearvikar19@users.noreply.github.com>
2023-05-01 17:43:53 +0530

    Add files via upload

---
+++ Lesson1/Basic/dist/index.ea47b41e.js
@@ -0,0 +1,6 @@
+const heading = document.createElement("h1");
+heading.innerHTML = "Welcome to React";
+const root = document.getElementById("root");
+root.appendChild(heading);
+
+//# sourceMappingURL=index.ea47b41e.js.map
